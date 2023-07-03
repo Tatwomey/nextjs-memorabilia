66cc416bbe7d0988ca619f6e52bad56248065ab0
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-07-03 19:07:42 -0400

    changes

--- components/Layout.js
+++ components/Layout.js
@@ -114,7 +114,7 @@ export default function Layout({ title, children }) {
                       </Menu.Item>
                     )}
                     <Menu.Item>
-                      <a legacyBehavior
+                      <a legacybehavior
                         className="dropdown-link"
                         href="#"
                         onClick={logoutClickHandler}
