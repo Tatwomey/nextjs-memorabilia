de8c5237044f4c88fa01c77d37ac74fbbef98876
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-07-04 20:59:30 -0400

    another one

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Product from '@/Models/Product';
 import db from '@/utils/db';
 import { Store } from '@/utils/Store';
 import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
 import Link from 'next/link';
 import Image from 'next/image';
 
