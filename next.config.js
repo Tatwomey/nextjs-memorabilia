b3ab747791037d4cd6a911ebf7f8446db27142a3
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-07-13 19:13:21 -0400

    changes

--- next.config.js
+++ next.config.js
@@ -3,34 +3,6 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/analytics',
-        destination: 'https://www.googletagmanager.com/gtag/js?id=YOUR_TRACKING_ID',
-      },
-    ];
-  },
-  head: {
-    script: [
-      {
-        src: '/analytics',
-        async: true,
-      },
-      {
-        dangerouslySetInnerHTML: {
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-KJMZD1YEY0');
-          `,
-        },
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
-
