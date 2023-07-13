456a39d906ba39b37428ceff38cb3ccacb9a3dd5
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-07-13 19:04:52 -0400

    document.js for GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,11 @@
 import '../styles/globals.css';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 import { SessionProvider, useSession } from 'next-auth/react';
 import { StoreProvider } from '../utils/Store';
+import { useRouter } from 'next/router';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
-import ReactGA from 'react-ga';
-
-ReactGA.initialize('G-KJMZD1YEY0'); // Replace 'YOUR_TRACKING_ID' with your actual tracking ID
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ReactGA.set({ page: url });
-      ReactGA.pageview(url);
-    };
-
-    // Initialize Google Analytics on mount
-    ReactGA.pageview(window.location.pathname);
-
-    // Log each page change as the route changes
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    // Clean up the event listener
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, []);
 
   return (
     <SessionProvider session={session}>
---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-KJMZD1YEY0"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-KJMZD1YEY0');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
