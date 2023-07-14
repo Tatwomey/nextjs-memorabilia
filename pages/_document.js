d5c1099fbfe99b9847c6380a6a5467cf93167bb5
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-07-14 09:36:47 -0400

    google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -5,19 +5,30 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-KJMZD1YEY0"></script>
+          {/* Paste your Google Tag Manager code here */}
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-KJMZD1YEY0');
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','G-KJMZD1YEY0');
               `,
             }}
           />
         </Head>
         <body>
+          {/* Paste the Google Tag Manager noscript code here */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
+          {/* End of Google Tag Manager noscript code */}
           <Main />
           <NextScript />
         </body>
